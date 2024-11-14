36dfecb7c6542c17e57b8e3791c7578a3c83378a
Albert Sunyer <167452076+asunyer-ooptimo@users.noreply.github.com>
2024-11-14 01:15:08 +0100

    Update test1.php

--- views/site/test1.php
+++ views/site/test1.php
@@ -8,16 +8,16 @@ $this->title = 'Test 1 — ooptimo';
     <div class="body-content">
         <div class="row mt-5">
             <div class="col-md-12">
-                <h2>Test 1 — Consumir API</h2>
-	            <p>El objetivo es cargar datos de una API externa y mostrarlos en pantalla.</p>
-	            <p>Usaremos la "Fake API" <a href="https://jsonplaceholder.typicode.com/" target="_blank">JSONPlaceholder</a> para cargar datos.<br>Usa este endpoint: <code>https://jsonplaceholder.typicode.com/posts</code>, y muestra una lista en esta misma página con los datos obtenidos.</p>
+                <h2>Test 1 — Consume API</h2>
+	            <p>The goal is to load data from an external API and display it on screen.</p>
+		    <p>We will use the "Fake API" <a href="https://jsonplaceholder.typicode.com/" target="_blank">JSONPlaceholder</a> to load data.<br>Use this endpoint: <code>https://jsonplaceholder.typicode.com/posts</code>, and display a list on this same page with the data retrieved.</p>
 	            
-	            <h5>Ayuda</h5>
+	            <h5>Help</h5>
 	            <ul>
-		            <li>Estamos usando el framework MVC Yii v2: <a href="https://www.yiiframework.com/doc/guide/2.0/en" target="_blank">Guia</a>.</li>
-		            <li>La API externa es la de JSONPlaceholder: <a href="https://jsonplaceholder.typicode.com/guide/" target="_blank">Guia</a></li>
-		            <li>Los archivos relevantes estan en <code>/controllers</code>, <code>/models</code> y <code>/views</code>.</li>
-		            <li>Para los estilos puedes editar directamente el archivo <code>/web/css/site.css</code>.</li>
+		            <li>We are using the Yii v2 MVC framework: <a href="https://www.yiiframework.com/doc/guide/2.0/en" target="_blank">Guide</a>.</li>
+		            <li>The external API is JSONPlaceholder: <a href="https://jsonplaceholder.typicode.com/guide/" target="_blank">Guide</a>.</li>
+		            <li>Relevant files are located in <code>/controllers</code>, <code>/models</code>, and <code>/views</code>.</li>
+		            <li>For styles, you can edit the <code>/web/css/site.css</code> file directly.</li>
 	            </ul>
             </div>
         </div>
